9515cc706f2efe512dacd1229376044379178c8f
Alka Sah <alkasah485@gmail.com>
2024-07-22 13:32:02 -0700

    made model folder

---
+++ models/ExpenseModel.js
@@ -0,0 +1,49 @@
+const {Schema, model} = require ('mongoose');
+
+const expenseSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim:true,
+            maxlength: 50,
+        },
+        amount: {
+            type: Number,
+            required: true,
+            maxlength: 20,
+            trim: true,
+        },
+        type: {
+            type: String,
+            default:"expense",
+        },
+        date: {
+            type: Date,
+            required: true,
+            trim:true,
+        },
+        category: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            required:true,
+            maxlength:20,
+            trim:true
+        },
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+          //timestamps:true
+       }
+
+);
+const Expense = model('expense', expenseSchema);
+
+module.exports = Expense;
---
+++ models/IncomeModel.js
@@ -0,0 +1,49 @@
+const {Schema, model} = require ('mongoose');
+
+const incomeSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim:true,
+            maxlength: 50,
+        },
+        amount: {
+            type: Number,
+            required: true,
+            maxlength: 20,
+            trim: true,
+        },
+        type: {
+            type: String,
+            default:"expense",
+        },
+        date: {
+            type: Date,
+            required: true,
+            trim:true,
+        },
+        category: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            required:true,
+            maxlength:20,
+            trim:true
+        },
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+        //timestamps:true
+      }
+);
+
+const Income = model ('income', incomeSchema)
+
+module.exports = Income;
