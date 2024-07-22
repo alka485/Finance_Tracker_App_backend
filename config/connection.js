1b17d604fcb5af13cc55e40565d5969ef5dd7f7f
Alka Sah <alkasah485@gmail.com>
2024-07-22 13:21:48 -0700

    modification in config

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,14 @@
+const { log } = require('console');
 const {connect , connection } = require ('mongoose');
 
 const connectionString = 
-process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/expenseTracker';
+process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/financeTracker';
 
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+console.log('db connected');
+
+module.exports = connection;
