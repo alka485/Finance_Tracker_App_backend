8ea28c0454160e46ffc2b4d7e14c649c4b8670f4
Alka Sah <alkasah485@gmail.com>
2024-07-22 13:46:40 -0700

    added data

---
+++ utils/data.js
@@ -0,0 +1,16 @@
+const title = [
+    'Grocery Shopping',
+    'Salary',
+    'Electricity Bill',
+    'Internet Bill',
+    'Rent',
+    'Dining Out',
+    'Freelance Project',
+    'Car Maintenance',
+    'Subscription',
+    'Gym Membership',
+]
+
+const category = [
+    
+]
