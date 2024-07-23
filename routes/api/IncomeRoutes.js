24b13299f5e9f4c3990a10470440025949ff8e10
Alka Sah <alkasah485@gmail.com>
2024-07-23 10:33:39 -0700

    modification saved

--- routes/api/IncomeRoutes.js
+++ routes/api/IncomeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const {
     getIncome,
     addIncome,
+    updateIncome,
     deleteIncome
 } = require("../../controllers/income");
 
@@ -9,6 +10,8 @@ const {
 router.route('/').get(getIncome).post(addIncome);
 
 //   /api/income/:incomeId
-router.route('/:incomeid').delete(deleteIncome);
+router.route('/:incomeid')
+.put(updateIncome)
+.delete(deleteIncome);
 
 module.exports = router;
