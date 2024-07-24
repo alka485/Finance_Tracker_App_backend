d4bd6afab2c871067a42f9e5c247d81f80ff5b47
Alka Sah <alkasah485@gmail.com>
2024-07-24 13:10:45 -0700

    save modification

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req,res) => {
     res.send("Welcome")
