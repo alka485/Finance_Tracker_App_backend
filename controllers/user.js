e634a808595622e6e6446422b6ccd2dbdd2ddc16
Alka Sah <alkasah485@gmail.com>
2024-07-29 09:52:57 -0700

    modification saved

---
+++ controllers/user.js
@@ -0,0 +1,43 @@
+const User = require('../models/UserModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+
+exports.registerUser = async(req,res) => {
+    const{email,password} = req.body;
+
+    try {
+        let user = await User.findOne({email});
+        if (user)
+            return res.status(400).json({msg: 'User already exist'});
+        
+        user = new User({
+            email,
+            password,
+        })
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+         const payload = {
+            user: {
+           id: user.id,
+             },
+        };
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+
+};
