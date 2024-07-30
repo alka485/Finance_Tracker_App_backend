06097fc2faca07a836cb856fd6a29875b2873430
Alka Sah <alkasah485@gmail.com>
2024-07-30 10:35:19 -0700

    modification saved

--- controllers/income.js
+++ controllers/income.js
@@ -14,9 +14,9 @@ module.exports = {
         (req,res)=> {
              // Check for validation errors
          //const errors = validationResult(req);
-         if (!errors.isEmpty()) {
-         return res.status(400).json({ errors: errors.array() });
-        } 
+        //  if (!errors.isEmpty()) {
+        //  return res.status(400).json({ errors: errors.array() });
+        // } 
         Income.create(req.body)
         .then((income) => res.json(income))
         .catch((err) => {
